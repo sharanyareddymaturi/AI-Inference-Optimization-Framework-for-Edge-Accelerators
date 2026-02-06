0265e57fdc5fc72f481a162cc3caa9b298faf636
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-02-06 09:23:00 +0530

    Add files via upload

---
+++ Accelerator.java
@@ -0,0 +1,9 @@
+package com.edgeai.accelerator;
+
+public interface Accelerator {
+    String getName();
+    double getComputePower();
+    double getMemoryBandwidth();
+    double getPowerConsumption();
+}
+
