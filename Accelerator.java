5e08c348751c14764a0d474c9d556d4585cee907
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-02-06 09:41:09 +0530

    Delete Accelerator.java

--- Accelerator.java
+++
@@ -1,9 +0,0 @@
-package com.edgeai.accelerator;
-
-public interface Accelerator {
-    String getName();
-    double getComputePower();
-    double getMemoryBandwidth();
-    double getPowerConsumption();
-}
-
